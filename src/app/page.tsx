528d5d85467d94f5d8a7d93257f014672da4242c
batmend2412 <batmend.battur@gmail.com>
2025-04-01 12:03:51 +0800

    mendeesda 4.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import { Input } from "@/components/ui/input";
-import { FaMoon } from "react-icons/fa";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { BsThreeDots } from "react-icons/bs";
+
 import Image from "next/image";
 import { Feature, Header, Popular, Toprated, Upcoming } from "@/components";
 export default function Page() {
   return (
     <div className="flex flex-col">
+      <p className="text-pink-400 text-2xl flex justify-center items-center">
+        Hairtai shuu bagshaa bas angiinhandaa vzvvleh ym danch alg hha ghde bvi
+        hiine hha
+      </p>
       <div>
         <Header />
         <Feature />
